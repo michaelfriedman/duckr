7f608dc1827961fb555b1425bb69c7f7a062a4c5
Michael Friedman <contact@michaeldavidfriedman.com>
2016-09-15 08:31:05 -0700

    Authenticate link and home link in navigation are now functional. State change upon isAuthed logic.

--- app/config/routes.js
+++ app/config/routes.js
@@ -4,10 +4,10 @@ import { MainContainer, HomeContainer, AuthenticateContainer } from 'containers'
 
 const routes = (
   <Router history={hashHistory}>
-    <Router path='/' component={MainContainer}>
+    <Route path='/' component={MainContainer}>
       <Route path='auth' component={AuthenticateContainer} />
       <IndexRoute component={HomeContainer} />
-    </Router>
+    </Route>
   </Router>
 )
 
