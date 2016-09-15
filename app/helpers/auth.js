38b8177f7ac283b90b8c09a61b9c9d34f87c13d5
Michael Friedman <contact@michaeldavidfriedman.com>
2016-09-15 00:02:05 -0700

    Authentication nearly finished.

---
+++ app/helpers/auth.js
@@ -0,0 +1,9 @@
+export default function auth () {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => resolve({
+      name: 'Michael Friedman',
+      avatar: 'https://avatars2.githubusercontent.com/u/17555926?v=3&s=400',
+      uid: 'the-uid'
+    }), 2000)
+  })
+}
